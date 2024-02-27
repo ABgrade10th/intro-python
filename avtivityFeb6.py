d95d3dbe156a4842b2e059a88ba2c0ae3fb99119
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-02-27 15:57:08 +0000

    Done

---
+++ avtivityFeb6.py
@@ -0,0 +1,29 @@
+# what is complexity ?
+# A: The measure of how resource
+# intensive a program can be.
+
+# Q: what is abstraction ?
+# A: Abstraction is process of
+# simplifying or hiding complexity.
+
+
+
+
+
+
+
+print(dataFromUser)
+
+
+
+
+name = input("what is your")
+teeshshirtSize=input('what is your tee shirt')
+teeShirtizeSm= False
+teeShirtSizeMd= False
+teeShirtSizelg= True
+
+chararaterObject = [3903, 'soft']
+chararaterObject_Movement=''
+chararaterObject_Attack=''
+chararaterObject_Interaction=''

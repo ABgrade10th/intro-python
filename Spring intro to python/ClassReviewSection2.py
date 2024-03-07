47d7634101a5ac54ca80a5e0e40a78ca7dc3b1e3
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-03-07 16:21:41 +0000

    Work is done!!

---
+++ Spring intro to python/ClassReviewSection2.py
@@ -0,0 +1,40 @@
+# PYTHON CLASS REVIEW
+
+# Vocab
+
+#1. Programs = Instructions for computers.
+
+
+#2 Programming Lauguages = Different ways we can provide
+# instructions to computers.
+
+#3. Syntax = The formatting rules of a language.
+# In this case, a programming language.
+
+#4. Data Types = The basic building blocks for our
+#programing language basically our
+# progaming languages' alphabet.
+
+
+#5. Variable = containers for data
+#(data are just data types.)
+
+
+# additional note - When we are writing programs, all
+# we're doing is talking to the computer and giving
+# it instructions to complete a task.
+
+
+# how to write a fucntion in Python
+
+def myFucntion(data):
+    print("Here are my  instructions...")
+
+# how to write a fucntion in JavaScript
+    
+function myFucntion(data)
+console.log(Here are my Inatructions...)
+
+
+
+

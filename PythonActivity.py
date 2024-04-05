6a55d21bd7efeed246c69959ce93ca29fa7ecbe8
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-04-05 15:24:12 +0000

    Done

---
+++ PythonActivity.py
@@ -0,0 +1,12 @@
+def passcode():
+    correctCode = 1234
+    userInput= int(input('Please enter a passcode:'))
+    
+    while userinput != correctCode:
+        print('attempt incorrect. Please try again.')
+        userInput=int(input('please enter correct code:')
+    if userInput == correctCode:
+        print('correct, access granted.')
+
+
+passcode()

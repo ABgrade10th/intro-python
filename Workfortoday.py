03e2a4a0a705215defa66a0a4b05a65a6c76f6fd
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-04-11 15:20:42 +0000

    Done

---
+++ Workfortoday.py
@@ -0,0 +1,74 @@
+# FOR LOOP - A construct that will repeat a set of instructions 
+# for a list/ group of data.
+
+birdGame = ['duck' , 'duck' , 'goose' , 'duck' , 'duck' ,]
+
+for x in birdGame:
+    if x in =='goose':
+        print(f"found the goose at index  {birdGame['goose']}")
+        continue
+    # for loops will only run
+    # up to the number of item in a group
+    print(bird)
+goFish = [Spades ]
+print(goFish)
+
+for card in Spades:
+    if card == Spades.index('SK'):
+        print('You have pulled the king of spade card.')
+     continue
+
+
+
+
+# white x == birdGame[0]: 
+     # while loops will run forever
+     # so long as the conditio is true.     
+     #print(x)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

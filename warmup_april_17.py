07496efbf6dafb9a96ba4eeef4828fe63325a8d0
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-04-17 15:02:16 +0000

    warmup done

---
+++ warmup_april_17.py
@@ -0,0 +1,82 @@
+
+# Warm up Tuesday April 16th, 2024.
+# create a python file called warmup_april_17.py
+import random
+
+# 1. In your own words, explain the difference between an Python For Loop and a
+# Python While Loop.
+
+# 2. Create a loop that will iterate over a list of numbers. For evey number in your loop,
+# it should multiply that number by 3. 
+
+numbers = [1,2,3,4,5,6,7]
+
+for number in numbersList: # for every item in this list
+    print(number *3) # multiply that item by 3 
+
+
+
+
+# 3. Use the following code snippet below to create a guessing game function. 
+# The code below will generate a random number for you. You must write a loop that will
+# ask the user to input their guess, if they guess incorrectly, the program will repeat 
+# itself and ask the user to guess again. The program should continue to ask them to
+# guess until they've gotten it correctly. Once they guess the correct number the program
+# will congratulate them and the loop will stop. 
+
+# generates a random number between 1 and 10
+randomNumber = random.randint(1, 10)                     
+
+print(f'Random number value is: {randomNumber}')
+
+# clues
+#we're using a while loop- program will repeat itsself; if 
+# the b=number is not the correct guess.
+# we'll need to create a function to store it.
+# we'll use the input function to take the users guess.
+
+def guessingGame():
+    randomNumber = randon.randint(1, 10)
+# print(f'Random number valueis:{randomNumber}')
+    i = int(input('please guess a number:'))
+
+    while i == randomNumber: # while this true...
+         print('Incorrect guess. Try again. ')
+         i = int(input('Please guess another number: '))
+         if i == randomNumber:
+             print('Congrats! Youve won the game.')
+
+guessingGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

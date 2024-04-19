2b0c9a7232709cbe3aaca44ca3831cb1d56d9d12
ABgrade10th <158069802+ABgrade10th@users.noreply.github.com>
2024-04-19 15:18:41 +0000

    done 04/19/24

---
+++ pythonRevewActivity_April19.py
@@ -0,0 +1,51 @@
+#1A.  Visit the following web page and read the article on asking questions.
+#1A. Visit the web page link below and read the following article on asking questions:
+# https://www.betterup.com/blog/how-to-ask-good-questions
+
+
+# 1B. Based on what you've read, in your own words, describe what a good
+# question is.
+
+'You might think that a good question returns the correct answer the first time '
+
+# 1C. In your opinion and in your own words, why do you think its
+# important to be able to ask good questions.
+
+'Questioning forces us to make important decisions'
+
+
+#2A. Visit the following webpage and complete the learning style survey
+# 2A. Visit the link web page below and complete the following learning style survey:
+# http://www.educationplanner.org/students/self-assessments/learning-styles-quiz.shtml
+
+#2B. Based on your completed survey results, which learning style are you? List, three 
+# things from the report that you believe are true. 
+#2B. Based on your completed survey results, which learning style are you? List, three (3)
+# things from the survey results that you feel best describe your learning style. 
+
+'Im a different style person i get on my phone and finish my work i be taking breaks sometimes i be needed them '
+
+#2B. Based on your completed survey results, do you feel as though the result are arcurate in
+# reflecting your learning style, if yes, explain why, if no, explain why. Please write your
+# response in complete sentences. 
+# reflecting your learning style? if yes, explain why, if no, explain why. Please write your
+# response in atleast three (3) complete sentences. 
+
+'No because i need help with alot of stuff in this class'
+
+# 3A. Visit the following webpage and read the article on how to study
+# 3A. Visit the  webp age link below and read the article on how to study:
+# https://summer.harvard.edu/blog/top-10-study-tips-to-study-like-a-harvard-student/
+
+# 3B. Which of the following tips do you feel you need to work on when studying? Explain
+# why in complete sentences.
+# why in at least three (3) complete sentences.
+
+'everything in this class hard i really dont know how to do alot of stuff on computer. '
+
+# 3C. Based on your learning style results, and the arcticle you just read on study tips
+# 3C. Based on your learning style results, and the arcticle you just read on study tips,
+# think about how you can apply the study tips to your learning style. Then, in complete sentences
+# write down four (4) steps you could take to effectively study that is specific you you. 
+
+
